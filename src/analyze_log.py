a1b892407c90599dd2d697419cf0ea2a2f58790f
humberto.dutra@hotmail.com <humberto.dutra@hotmail.com>
2023-02-13 15:46:22 -0300

    exer 1,2

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,61 @@
+import csv
+from collections import Counter
+from os import path
+
+
+def favorite_meal_func(data, name):
+    meals = [meal[1] for meal in data if meal[0] == name]
+    favorite_meal = Counter(meals).most_common(1)[0][0]
+    return favorite_meal
+
+
+def how_many_times_func(data, name, meal):
+    how_many_times = len(
+        [order for order in data if order[0] == name and order[1] == meal])
+    return how_many_times
+
+
+def which_meal_never_ordered(data, name):
+    person_meals = [meal[1] for meal in data if meal[0] == name]
+    all_meals = set([meal[1] for meal in data])
+    meals_never_ordered = all_meals - set(person_meals)
+    return meals_never_ordered
+
+
+def days_that_never_ordered(data, name):
+    days = set()
+    for order in data:
+        if order[0] == name:
+            days.add(order[2])
+
+    all_days = set(day[2] for day in data)
+    never_ordered_days = all_days - days
+
+    return never_ordered_days
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    if '.csv' not in path_to_file:
+        raise FileNotFoundError(f"Extensão inválida: {path_to_file}")
+
+    if not path.exists(path_to_file):
+        raise FileNotFoundError(f'Arquivo inexistente: {path_to_file}')
+
+    with open(path_to_file, 'r') as file:
+        orders = list(csv.reader(file))
+        favorite_meal = favorite_meal_func(orders, 'maria')
+        how_many_times = how_many_times_func(orders, 'arnaldo', 'hamburguer')
+        meals_never_ordered = which_meal_never_ordered(orders, 'joao')
+        days_never_ordered = days_that_never_ordered(orders, 'joao')
+        result = (
+            f"{favorite_meal}\n"
+            f"{how_many_times}\n"
+            f"{meals_never_ordered}\n"
+            f"{days_never_ordered}"
+        )
+        with open("data/mkt_campaign.txt", "w") as file:
+            file.write(result)
+
+
+# path_ = './data/orders_1.csv'
+# analyze_log(path_)
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,22 +1,61 @@
+from collections import defaultdict
+
+
 class TrackOrders:
     # aqui deve expor a quantidade de estoque
-    def __len__(self):
-        pass
+    def __init__(self):
+        self.orders = []
 
-    def add_new_order(self, customer, order, day):
-        pass
+    def __len__(self):
+        return len(self.orders)
+
+    def add_new_order(self, customer: str, order: str, day: str) -> None:
+        self.orders.append(
+            {"nome": customer, "prato": order, "dia_da_semana": day}
+        )
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        data = self.orders
+        filtred_data = [order for order in data if order["nome"] == customer]
+        plate_counts = {}
+        for order in filtred_data:
+            plate = order["prato"]
+            if plate in plate_counts:
+                plate_counts[plate] += 1
+            else:
+                plate_counts[plate] = 1
+        return max(plate_counts, key=plate_counts.get)
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        data = self.orders
+        customer_orders = [item for item in data if item['nome'] == customer]
+        customer_plates = [item['prato'] for item in customer_orders]
+        all_plates = set([item['prato'] for item in data])
+
+        never_ordered_plates = all_plates - set(customer_plates)
+
+        return never_ordered_plates
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        data = self.orders
+        customer_orders = [item for item in data if item['nome'] == customer]
+        customer_days = [item['dia_da_semana'] for item in customer_orders]
+        all_days = set([item['dia_da_semana'] for item in data])
+
+        never_ordered_days = all_days - set(customer_days)
+
+        return never_ordered_days
 
     def get_busiest_day(self):
-        pass
+        data = self.orders
+        days_count = defaultdict(int)
+        for entry in data:
+            days_count[entry['dia_da_semana']] += 1
+        return max(days_count, key=days_count.get)
 
     def get_least_busy_day(self):
-        pass
+        data = self.orders
+        days_count = defaultdict(int)
+        for entry in data:
+            days_count[entry['dia_da_semana']] += 1
+        return min(days_count, key=days_count.get)
